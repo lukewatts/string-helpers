80bc283b134c7e2da878d93b7eefe527445711ee
Luke Watts <luke@luke-watts.com>
2017-06-13 09:24:56 +0100

    Adding str_contains function. Checks if str contains a substring

---
+++ str-contains.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!function_exists('str_contains')) {
+	 /**
+     * Determine if a given string contains a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     * @return bool
+     */
+    function str_contains($needles, $haystack)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle != '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
