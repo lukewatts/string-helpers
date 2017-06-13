202493e9d673bdf1a5c119fa6b8bc89bdd397a74
Luke Watts <luke@luke-watts.com>
2017-06-13 10:34:43 +0100

    Fixing classname title_case to ucwords

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# string-helpers
+# String Helpers
 Helper functions for string manipulation
+
+## classname
+
+Get the class name without namespace.
+
+```php
+classname('App\\Model\\User'); // User
+```
+
+Can also be used to convert formward slashed namespaces to correct namespaces.
+
--- classname.php
+++ classname.php
@@ -2,17 +2,33 @@
 
 if (!function_exists('classname')) {
     /**
-    * Get the class name without namespace
-    *
-    * @author Luke Watts
-    *
-    * @param  string|object  $class
-    *
-    * @return string
-    */
+     * Get the class name without namespace
+     *
+     * @author Luke Watts
+     *
+     * @param  string|object $class
+     * @param  bool          $namespace
+     *
+     * @return string
+     */
     function classname($class, $namespace = false)
     {
         $class = is_object($class) ? get_class($class) : $class;
-        retrun ($namespace) ? $class; : basename(str_replace('\\', '/', $class));
+
+        if ($namespace) {
+            $class = str_replace(['/', '-', '_'], ' ', $class);
+
+            $title_case = ucwords($class);
+            $explode = explode(' ', $title_case);
+
+            $remove_empties = array_filter($explode, function ($word) {
+                return (!empty($word));
+            });
+
+            return implode('\\', $remove_empties);
+        } else {
+            return basename(str_replace('\\', '/', $class));
+        }
+
     }
 }
